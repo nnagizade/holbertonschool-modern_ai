897086e31a4222b4da356237566131914a6f041a
nnagizade <nagizadenigar05@gmail.com>
2026-06-13 17:48:02 +0000

    task 4

--- machine_learning/unsupervised_learning/4-agglomerative.py
+++ machine_learning/unsupervised_learning/4-agglomerative.py
@@ -18,13 +18,9 @@ def Agglomerative(X, n_clusters):
     Returns:
     - numpy.ndarray: Cluster labels for each data point of shape (n_samples,)
     """
-    # Create the link matrix using Ward's linkage method
     linkage_matrix = sch.linkage(X, method='ward')
-
-    # Plot the dendrogram using scipy
     sch.dendrogram(linkage_matrix)
 
-    # Fit the agglomerative clustering model
     agg_model = cluster.AgglomerativeClustering(
         n_clusters=n_clusters,
         linkage='ward'
